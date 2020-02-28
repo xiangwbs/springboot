1d8aec4d62f09acb2a37c54f840e5be24a4d001d
xiangwb <xiangwbs@163.com>
2020-02-28 15:56:18 +0800

    添加花呗分期

--- boot-service/src/main/java/com/xwbing/domain/entity/pay/alipay/AliPayTradePayParam.java
+++ boot-service/src/main/java/com/xwbing/domain/entity/pay/alipay/AliPayTradePayParam.java
@@ -20,7 +20,7 @@ public class AliPayTradePayParam {
      * 条码支付:bar_code
      * 声波支付:wave_code
      */
-    private String scene;
+    private String scene = "bar_code";
     /**
      * 支付授权码
      */
@@ -47,6 +47,12 @@ public class AliPayTradePayParam {
      */
     @JSONField(name = "timeout_express")
     private String timeoutExpress = "10m";
+    /**
+     * hb_fq_num 花呗分期数 仅支持传入 3、6、12
+     * hb_fq_seller_percent 卖家承担收费比例 商家承担手续费传入 100，用户承担手续费传入 0，仅支持传入 100、0 两种
+     */
+    @JSONField(name = "extend_params")
+    private String extendParams;
 
     public AliPayTradePayParam(String outTradeNo, String authCode, String subject, String body, float totalAmount) {
         this.outTradeNo = outTradeNo;
