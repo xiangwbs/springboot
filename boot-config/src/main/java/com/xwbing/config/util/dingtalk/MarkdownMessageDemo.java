75d4b69e29a220e08a09bf7a7e8942a494c61844
xiangwb <xiangwbs@163.com>
2020-07-09 21:29:05 +0800

    删除图片

--- boot-config/src/main/java/com/xwbing/config/util/dingtalk/MarkdownMessageDemo.java
+++ boot-config/src/main/java/com/xwbing/config/util/dingtalk/MarkdownMessageDemo.java
@@ -41,7 +41,7 @@ public class MarkdownMessageDemo {
     public void testSendChatMessage() throws Exception {
         MarkdownMessage message = new MarkdownMessage();
         message.setChatId("chat0f212fb4dc07478f0813eb98e9470ff6");
-        message.setCover(MarkdownMessage.getImageText("https://oss-edu-test.dingtax.cn/cover/idNzvQCmnBTB8YYsAPBdVHJi.jpg"));
+        message.setCover(MarkdownMessage.getImageText("https://gw.alicdn.com/tfs/TB1ut3xxbsrBKNjSZFpXXcXhFXa-846-786.png"));
         message.setTitle("通知公告测试");
         //title当做一级标题
         message.addItem(1, MarkdownMessage.getHeaderText(2, message.getTitle()));
