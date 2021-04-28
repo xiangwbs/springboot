a73dab4451b1d2bd2a0622ec81b974484576c2f8
xiangwb <xiangwbs@163.com>
2021-04-28 15:27:20 +0800

    修改频率限制切面

--- boot-starter/src/main/java/com/xwbing/starter/aspect/LimitAspect.java
+++ boot-starter/src/main/java/com/xwbing/starter/aspect/LimitAspect.java
@@ -35,7 +35,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Aspect
 public class LimitAspect {
-    public static final String LIMIT_KEY_PREFIX = "boot:limit_";
+    private static final String LIMIT_KEY_PREFIX = "boot:limit_";
     @Resource
     private RedisService redisService;
 
