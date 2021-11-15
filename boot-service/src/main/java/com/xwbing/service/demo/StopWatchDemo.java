53bfb7519f7d20c1d18d01c4fe54973628ba4d54
xiangwb <xiangwbs@163.com>
2021-11-15 15:59:14 +0800

    fix

--- boot-service/src/main/java/com/xwbing/service/demo/StopWatchDemo.java
+++ boot-service/src/main/java/com/xwbing/service/demo/StopWatchDemo.java
@@ -2,10 +2,12 @@ package com.xwbing.service.demo;
 
 import org.springframework.util.StopWatch;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author xiangwb
- * @date 2020/3/5 23:06
  */
+@Slf4j
 public class StopWatchDemo {
     public static void main(String[] args) throws InterruptedException {
         StopWatch sw = new StopWatch("test");
@@ -13,13 +15,11 @@ public class StopWatchDemo {
         // do something
         Thread.sleep(100);
         sw.stop();
-        System.out.println(sw.getTotalTimeMillis());
         sw.start("task2");
         // do something
         Thread.sleep(200);
         sw.stop();
-        System.out.println(sw.getTotalTimeMillis());
-        // System.out.println(sw.shortSummary());
+        // System.out.println(sw.getTotalTimeMillis());
         System.out.println(sw.prettyPrint());
     }
 }
