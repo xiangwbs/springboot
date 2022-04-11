51a6f9baf1bffdcc4c1b67568dee8db7f4460fda
xiangwb <xiangwbs@163.com>
2022-04-11 21:46:27 +0800

    fix

--- boot-web/src/main/java/com/xwbing/web/controller/wx/WxMpPortalController.java
+++ boot-web/src/main/java/com/xwbing/web/controller/wx/WxMpPortalController.java
@@ -9,21 +9,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.xwbing.service.exception.BusinessException;
 import com.xwbing.web.response.ApiResponse;
 import com.xwbing.web.response.ApiResponseUtil;
 
+import cn.hutool.core.util.IdUtil;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.common.api.WxConsts.EventType;
+import me.chanjar.weixin.common.api.WxConsts.XmlMsgType;
 import me.chanjar.weixin.common.bean.WxJsapiSignature;
 import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
 import me.chanjar.weixin.common.bean.oauth2.WxOAuth2AccessToken;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.common.service.WxOAuth2Service;
 import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpQrcodeService;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import me.chanjar.weixin.mp.bean.result.WxMpUser;
 
 /**
  * @author Binary Wang(https://github.com/binarywang)
@@ -37,61 +44,61 @@ public class WxMpPortalController {
     private final WxMpMessageRouter wxMpMessageRouter;
 
     @GetMapping(produces = "text/plain;charset=utf-8")
-    public String authGet(@PathVariable String appId,
-            @RequestParam(name = "signature", required = false) String signature,
-            @RequestParam(name = "timestamp", required = false) String timestamp,
-            @RequestParam(name = "nonce", required = false) String nonce,
-            @RequestParam(name = "echostr", required = false) String echostr) {
-        log.info("接收到来自微信服务器的认证消息：[{}, {}, {}, {}]", signature, timestamp, nonce, echostr);
+    public String authGet(@PathVariable String appId, @RequestParam(required = false) String timestamp,
+            @RequestParam(required = false) String nonce, @RequestParam(required = false) String signature,
+            @RequestParam(required = false) String echostr) {
+        log.info("WxMpPortalController authGet timestamp:{} nonce:{} signature:{} echostr:{}", timestamp, nonce,
+                signature, echostr);
         if (StringUtils.isAnyBlank(signature, timestamp, nonce, echostr)) {
+            log.error("WxMpPortalController authGet error param");
             throw new IllegalArgumentException("请求参数非法，请核实!");
         }
-        // if (!this.wxService.switchover(appid)) {
-        //     throw new IllegalArgumentException(String.format("未找到对应appid=[%s]的配置，请核实！", appid));
-        // }
         if (wxMpService.checkSignature(timestamp, nonce, signature)) {
             return echostr;
+        } else {
+            log.error("WxMpPortalController authGet checkSignature fail");
         }
         return "非法请求";
     }
 
     @PostMapping(produces = "application/xml; charset=UTF-8")
-    public String post(@PathVariable String appId, @RequestBody String requestBody,
-            @RequestParam("signature") String signature, @RequestParam("timestamp") String timestamp,
-            @RequestParam("nonce") String nonce, @RequestParam("openid") String openid,
+    public String post(@PathVariable String appId, @RequestParam String timestamp, @RequestParam String nonce,
+            @RequestParam String signature, @RequestParam String openid, @RequestBody String requestBody,
             @RequestParam(name = "encrypt_type", required = false) String encType,
             @RequestParam(name = "msg_signature", required = false) String msgSignature) {
-        log.info("接收微信请求：[openid=[{}], [signature=[{}], encType=[{}], msgSignature=[{}],"
-                        + " timestamp=[{}], nonce=[{}], requestBody=[\n{}\n] ", openid, signature, encType, msgSignature,
-                timestamp, nonce, requestBody);
-        // if (!this.wxService.switchover(appid)) {
-        //     throw new IllegalArgumentException(String.format("未找到对应appid=[%s]的配置，请核实！", appid));
-        // }
+        log.info(
+                "WxMpPortalController post timestamp:{} nonce:{} signature:{} encType:{} msgSignature:{} openid:{} requestBody:{}",
+                timestamp, nonce, signature, encType, msgSignature, openid, requestBody);
         if (!wxMpService.checkSignature(timestamp, nonce, signature)) {
+            log.error("WxMpPortalController post checkSignature fail");
             throw new IllegalArgumentException("非法请求，可能属于伪造的请求！");
         }
-        String out = null;
+        // String out = null;
         if (encType == null) {
             // 明文传输的消息
             WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(requestBody);
-            WxMpXmlOutMessage outMessage = this.route(inMessage);
-            if (outMessage == null) {
-                return "";
-            }
-            out = outMessage.toXml();
+            this.handle(inMessage);
+            // WxMpXmlOutMessage outMessage = this.route(inMessage);
+            // if (outMessage == null) {
+            //     return "";
+            // }
+            // out = outMessage.toXml();
         } else if ("aes".equalsIgnoreCase(encType)) {
             // aes加密的消息
             WxMpXmlMessage inMessage = WxMpXmlMessage
                     .fromEncryptedXml(requestBody, wxMpService.getWxMpConfigStorage(), timestamp, nonce, msgSignature);
-            log.debug("\n消息解密后内容为：\n{} ", inMessage.toString());
-            WxMpXmlOutMessage outMessage = this.route(inMessage);
-            if (outMessage == null) {
-                return "";
-            }
-            out = outMessage.toEncryptedXml(wxMpService.getWxMpConfigStorage());
+            log.info("WxMpPortalController post decryptedXml:{}", inMessage.toString());
+            this.handle(inMessage);
+            // log.debug("\n消息解密后内容为：\n{} ", inMessage.toString());
+            // WxMpXmlOutMessage outMessage = this.route(inMessage);
+            // if (outMessage == null) {
+            //     return "";
+            // }
+            // out = outMessage.toEncryptedXml(wxMpService.getWxMpConfigStorage());
         }
-        log.debug("\n组装回复信息：{}", out);
-        return out;
+        // log.debug("\n组装回复信息：{}", out);
+        // return out;
+        return "success";
     }
 
     private WxMpXmlOutMessage route(WxMpXmlMessage message) {
@@ -101,6 +108,43 @@ public class WxMpPortalController {
             log.error("路由消息时出现异常！", e);
         }
         return null;
+    }
+
+    public void handle(WxMpXmlMessage message) {
+        String msgType = message.getMsgType();
+        if (XmlMsgType.EVENT.equals(msgType)) {
+            this.handleEvent(message);
+        }
+    }
+
+    private void handleEvent(WxMpXmlMessage message) {
+        WxMpUser wxMpUser;
+        String openId = message.getFromUser();
+        try {
+            wxMpUser = wxMpService.getUserService().userInfo(openId);
+        } catch (WxErrorException e) {
+            log.error("WxMpPortalController handleEvent errorCode:{} errorMsg:{}", e.getError().getErrorCode(),
+                    e.getError().getErrorMsg());
+            return;
+        }
+        String event = message.getEvent();
+        String eventKey = message.getEventKey();
+        switch (event) {
+            case WxConsts.EventType.SUBSCRIBE:
+                if (StringUtils.isNotEmpty(eventKey) && eventKey.startsWith("qrscene_")) {
+                }
+                break;
+            case EventType.UNSUBSCRIBE:
+                break;
+            case EventType.SCAN:
+                if (Boolean.TRUE.equals(wxMpUser.getSubscribe())) {
+
+                }
+                break;
+            default:
+                break;
+
+        }
     }
 
     @ApiOperation("获取Jsapi签名")
@@ -115,5 +159,19 @@ public class WxMpPortalController {
         WxOAuth2Service oAuth2Service = wxMpService.getOAuth2Service();
         WxOAuth2AccessToken accessToken = oAuth2Service.getAccessToken(code);
         return ApiResponseUtil.success(oAuth2Service.getUserInfo(accessToken, null));
+    }
+
+    @ApiOperation("获取二维码")
+    @GetMapping("/getQrcode")
+    public String getMpQrcode() {
+        String sceneStr = IdUtil.fastSimpleUUID();
+        try {
+            WxMpQrcodeService qrcodeService = wxMpService.getQrcodeService();
+            String ticket = qrcodeService.qrCodeCreateTmpTicket(sceneStr, 60 * 10).getTicket();
+            return qrcodeService.qrCodePictureUrl(ticket);
+        } catch (WxErrorException e) {
+            log.error("getQrcode errorCode:{} errorMsg:{}", e.getError().getErrorCode(), e.getError().getErrorMsg());
+            throw new BusinessException("获取微信二维码失败");
+        }
     }
 }
