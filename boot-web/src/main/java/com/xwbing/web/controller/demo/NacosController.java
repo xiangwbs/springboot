e5d5c9f0a92a36e1059cd29bac0675e10dbfe6d6
xiangwb <xiangwbs@163.com>
2022-10-27 14:32:18 +0800

    nacos demo

--- boot-web/src/main/java/com/xwbing/web/controller/demo/NacosController.java
+++ boot-web/src/main/java/com/xwbing/web/controller/demo/NacosController.java
@@ -31,12 +31,17 @@ public class NacosController {
     @GetMapping("/registry")
     public void registry() throws NacosException {
         Instance instance = new Instance();
+        // 集群名称
         instance.setClusterName("boot");
         instance.setEnabled(true);
-        // AP（distro）、CP(Raft)
+        // 临时节点/持久节点， 临时节点是AP模式（采用distro算法），持久节点是CP（采用raft算法）
         instance.setEphemeral(true);
+        // 健康状态
+        instance.setHealthy(true);
         instance.setIp("127.0.0.1");
         instance.setPort(8080);
+        // 权重取值范围1到100，数值越大，权重越大
+        // instance.setWeight(4.0);
         namingService.registerInstance("boot-module-pro", instance);
     }
 
@@ -50,10 +55,24 @@ public class NacosController {
 
     /**
      * 订阅服务
+     *
+     * 当服务提供者出现服务扩容和缩容时，服务消费者需要感知到服务地址的变化，可以通过监听服务来动态感知到服务的变化
      */
     @GetMapping("/subscribe")
     public void subscribe(@RequestParam String serviceName) throws NacosException {
         namingService.subscribe(serviceName, event -> {
+            System.out.println("收到地址变更的事件");
+            NamingEvent namingEvent = (NamingEvent)event;
+            System.out.println(Jackson.build().writeValueAsString(namingEvent));
+        });
+    }
+
+    /**
+     * 取消订阅
+     */
+    @GetMapping("/unsubscribe")
+    public void unsubscribe(@RequestParam String serviceName) throws NacosException {
+        namingService.unsubscribe(serviceName, event -> {
             System.out.println("收到地址变更的事件");
             NamingEvent namingEvent = (NamingEvent)event;
             System.out.println(Jackson.build().writeValueAsString(namingEvent));
