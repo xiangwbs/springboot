bfad2b221db604721f7eaff2a10806513fe67961
xiangwb <xiangwbs@163.com>
2023-02-08 20:06:29 +0800

    fix

--- boot-starter/src/main/java/com/xwbing/starter/log/annotation/OperateLog.java
+++ boot-starter/src/main/java/com/xwbing/starter/log/annotation/OperateLog.java
@@ -16,13 +16,16 @@ import com.xwbing.starter.log.function.ICustomFunction;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OperateLog {
+    /**
+     * 标签
+     */
     String tag();
 
     /**
      * SpEL表达式: #p0 | #p0.field | #paramName | paramName.field
      * 语法:{functionName{SpEL}}|{{SpEL}}|SpEL
      * 自定义函数需继承{@link ICustomFunction} 示例查看{@link ExampleCustomerFunction}
-     * 特殊参数:操作人{{_operator}} 错误信息{{_errMsg}} 执行结果:{{#_result}}
+     * 特殊参数:操作人{{_operator}} 执行结果:{{#_result}} 错误信息{{_errMsg}}
      */
     String content();
 }
