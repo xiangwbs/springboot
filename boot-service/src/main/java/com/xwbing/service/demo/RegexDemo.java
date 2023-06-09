96006d6857cc1bd363cda81ee46fe239513581b4
xiangwb <xiangwbs@163.com>
2023-06-09 16:36:58 +0800

    fix

--- boot-service/src/main/java/com/xwbing/service/demo/RegexDemo.java
+++ boot-service/src/main/java/com/xwbing/service/demo/RegexDemo.java
@@ -39,8 +39,8 @@ public class RegexDemo {
         // 域名部分：由字母（大小写不限）、数字、点或减号组成。
         // 顶级域名：由两个或更多字母组成
         System.out.println("xiangwb@163.com".matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"));
-        // 以"http://"、"https://"或"ftp://"开头的网址。假设网址中不包含空格、斜杠、问号或井号，并且不以这些字符结尾
-        System.out.println("https://www.baidu.com".matches("^(?:https?|ftp)://[^\\s/$.?#].[^\\s]*$"));
+        // 以"http://"、"https://"或"ftp://"开头的网址。假设网址中不包含空格、斜杠、问号或井号，并且不以空格结尾
+        System.out.println("https://www.baidu.com".matches("^(https?|ftp)://[^\\s/$.?#].[^\\s]*$"));
         System.out.println(ReUtil.getGroup0("\\d+", "qaz1235qwer5678"));
         System.out.println(ReUtil.findAllGroup0("\\d+", "qaz1235qwer5678"));
         System.out.println(StrUtil.subBetween(ReUtil.getGroup0("\\$\\{\\w+}", "123${code}456"), "${", "}"));
