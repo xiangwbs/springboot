d51c2983a090756d10cedc04707b5995bec09e84
xiangwb <xiangwbs@163.com>
2023-11-09 09:36:30 +0800

    fix

--- boot-service/src/main/java/com/xwbing/service/util/dingtalk/DingTalkConstant.java
+++ boot-service/src/main/java/com/xwbing/service/util/dingtalk/DingTalkConstant.java
@@ -8,6 +8,6 @@ package com.xwbing.service.util.dingtalk;
 public class DingTalkConstant {
     public static final String CHAT_BASE_URL = "https://oapi.dingtalk.com/chat/send?access_token=%s";
     public static final String ROBOT_BASE_URL = "https://oapi.dingtalk.com/robot/send?access_token=%s";
-    public static final String WEBHOK = String.format(ROBOT_BASE_URL, "da82d4099a3f2515480f35210cc17dd02f315e99537d29cd3de1a751e551b670)");
+    public static final String WEBHOK = String.format(ROBOT_BASE_URL, "da82d4099a3f2515480f35210cc17dd02f315e99537d29cd3de1a751e551b670");
     public static final String SECRET = "SECa17df9ae897fb39d525a173c47dcc93a6b01133fbb4bf3e659060cd4ed4539ea";
 }
