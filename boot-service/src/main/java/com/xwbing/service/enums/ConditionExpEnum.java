56669feb38e946d52f0400c45dc9ad8affc75064
xiangwb <xiangwbs@163.com>
2023-11-27 15:22:42 +0800

    fix

--- boot-service/src/main/java/com/xwbing/service/enums/ConditionExpEnum.java
+++ boot-service/src/main/java/com/xwbing/service/enums/ConditionExpEnum.java
@@ -34,14 +34,29 @@ public enum ConditionExpEnum {
 
     private final String name;
 
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Condition {
+        // 条件名称
+        private String name;
+        // 条件key
+        private String key;
+        // 条件值
+        private String value;
+        // 条件表达式
+        private ConditionExpEnum expression;
+    }
+
     public static List<String> listConditionExp() {
         return Arrays.stream(ConditionExpEnum.values()).map(ConditionExpEnum::getName).collect(Collectors.toList());
     }
 
     /**
-     * @param ruleGroups dataMap里的数据 一组规则可以包含多个条件
-     * @param dataMap
-     * @return
+     * @param ruleGroups 规则组 一组规则可以包含多个条件
+     * @param dataMap    key:条件Key value:用户数据
+     * @return 匹配的规则组
      */
     public static List<Condition> match(List<List<Condition>> ruleGroups, Map<String, String> dataMap) {
         if (CollectionUtils.isEmpty(ruleGroups) || MapUtils.isEmpty(dataMap)) {
@@ -57,14 +72,14 @@ public enum ConditionExpEnum {
             for (Condition rule : ruleGroup) {
                 // 获取数据
                 String data = dataMap.get(rule.getKey());
-                // 获取不到匹配项视作匹配失败
-                matched = StringUtils.isNotEmpty(data) && rule(rule, data);
+                // 匹配规则
+                matched = rule(rule, data);
                 if (!matched) {
                     // 任意匹配失败则退出当前循环，开始匹配下个组
                     break;
                 }
             }
-            // 如果当前组匹配成功返回
+            // 如果当前组匹配成功则返回对应条件
             if (matched) {
                 return ruleGroup;
             }
@@ -73,12 +88,12 @@ public enum ConditionExpEnum {
     }
 
     private static boolean rule(Condition condition, String data) {
-        return StrUtil.isNumeric(data) ?
-                ConditionExpEnum.numberRule(Long.parseLong(data), condition.getValue(), condition.getExpression().getName()) :
-                ConditionExpEnum.stringRule(data, condition.getValue(), condition.getExpression().getName());
+        return StrUtil.isNumeric(data)
+                ? ConditionExpEnum.numberRule(Long.parseLong(data), condition.getValue(), condition.getExpression().getName())
+                : ConditionExpEnum.stringRule(data, condition.getValue(), condition.getExpression().getName());
     }
 
-    private static boolean numberRule(long value, String conditionValue, String conditionExp) {
+    private static boolean numberRule(Long value, String conditionValue, String conditionExp) {
         List<Long> numbers;
         if (JSONUtil.isTypeJSONArray(conditionValue)) {
             numbers = JSONUtil.toList(conditionValue, Long.class);
@@ -86,18 +101,19 @@ public enum ConditionExpEnum {
             numbers = ListUtil.toList(Long.valueOf(conditionValue));
         }
         if (numbers.size() == 1) {
+            Long number = numbers.get(0);
             if (EQ.getName().equals(conditionExp)) {
-                return value == numbers.get(0);
+                return Objects.equals(value, number);
             } else if (NE.getName().equals(conditionExp)) {
-                return value != numbers.get(0);
+                return !Objects.equals(value, number);
             } else if (GT.getName().equals(conditionExp)) {
-                return value > numbers.get(0);
+                return value > number;
             } else if (GE.getName().equals(conditionExp)) {
-                return value >= numbers.get(0);
+                return value >= number;
             } else if (LT.getName().equals(conditionExp)) {
-                return value < numbers.get(0);
+                return value < number;
             } else if (LE.getName().equals(conditionExp)) {
-                return value <= numbers.get(0);
+                return value <= number;
             }
         } else {
             if (IN.getName().equals(conditionExp)) {
@@ -118,19 +134,22 @@ public enum ConditionExpEnum {
         }
         if (strings.size() == 1) {
             if (EQ.getName().equals(conditionExp)) {
-                return value.equals(conditionValue);
+                return StringUtils.isNotEmpty(value) && value.equals(conditionValue);
             } else if (NE.getName().equals(conditionExp)) {
-                return !value.equals(conditionValue);
+                return StringUtils.isNotEmpty(value) && !value.equals(conditionValue);
             } else if (IS_NULL.getName().equals(conditionExp)) {
                 return StringUtils.isEmpty(value);
             } else if (IS_NOT_NULL.getName().equals(conditionExp)) {
                 return StringUtils.isNotEmpty(value);
             } else if (IN.getName().equals(conditionExp)) {
-                return value.contains(conditionValue);
+                return StringUtils.isNotEmpty(value) && value.contains(conditionValue);
             } else if (NOT_IN.getName().equals(conditionExp)) {
-                return !value.contains(conditionValue);
+                return StringUtils.isNotEmpty(value) && !value.contains(conditionValue);
             }
         } else {
+            if (StringUtils.isEmpty(value)) {
+                return false;
+            }
             if (IN.getName().equals(conditionExp)) {
                 return strings.stream().anyMatch(value::contains);
             } else if (NOT_IN.getName().equals(conditionExp)) {
@@ -142,30 +161,27 @@ public enum ConditionExpEnum {
 
     public static void main(String[] args) {
         List<List<Condition>> ruleGroups = new ArrayList<>();
+//        ruleGroups.add(ListUtil.toList(Condition.builder().name("名字").key("name").expression(ConditionExpEnum.IS_NOT_NULL).build()));
         ruleGroups.add(ListUtil.toList(Condition.builder().name("地址").key("address").value("[\"杭州\",\"上海\"]").expression(ConditionExpEnum.IN).build()));
         ruleGroups.add(ListUtil.toList(Condition.builder().name("排队数").key("wait").value("10").expression(ConditionExpEnum.GT).build(), Condition.builder().name("小休数").key("rest").value("10").expression(ConditionExpEnum.GT).build()));
         Map<String, String> dataMap = new HashMap<>();
         dataMap.put("wait", "15");
         dataMap.put("rest", "20");
         dataMap.put("address", "大杭州");
+        dataMap.put("name", "道风");
         List<Condition> matchList = match(ruleGroups, dataMap);
         if (CollectionUtils.isNotEmpty(matchList)) {
             String reason = matchList
                     .stream()
-                    .map(condition -> condition.getName() + condition.getExpression().getName() + "设定值" + condition.getValue())
-                    .collect(Collectors.joining(","));
+                    .map(condition -> {
+                        String content = condition.getName() + condition.getExpression().getName();
+                        if (StringUtils.isNotEmpty(condition.getValue())) {
+                            content += condition.getValue();
+                        }
+                        return content;
+                    })
+                    .collect(Collectors.joining(";"));
             System.out.println(reason);
         }
-    }
-
-    @Data
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class Condition {
-        private String name;
-        private String key;
-        private String value;
-        private ConditionExpEnum expression;
     }
 }
