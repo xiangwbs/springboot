9337110d12450540494231bca80ebc25bfa69c7d
xiangwb <xiangwbs@163.com>
2023-11-29 17:59:15 +0800

    fix

--- boot-service/src/main/java/com/xwbing/service/demo/PageDemo.java
+++ boot-service/src/main/java/com/xwbing/service/demo/PageDemo.java
@@ -1,22 +1,20 @@
 package com.xwbing.service.demo;
 
-import java.util.ArrayList;
+import cn.hutool.core.collection.CollUtil;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.ListUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.util.StopWatch;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections4.ListUtils;
-import org.springframework.util.StopWatch;
-
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
-
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author daofeng
@@ -26,7 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PageDemo {
 
-    public static void pageHelper(int pageSize) {
+    public static void selectByPageHelperAsync(int pageSize) {
         log.info("pageHelper start");
         Page<Object> page = PageHelper.startPage(1, 1);
         query();
@@ -41,7 +39,7 @@ public class PageDemo {
         log.info("pageHelper page:{}", times);
         ThreadPoolExecutor threadPool = new ThreadPoolExecutor(3, 3, 600L, TimeUnit.MICROSECONDS,
                 new LinkedBlockingQueue<>(), new ThreadFactoryBuilder().setNameFormat("pageHelper-pool-%d").build());
-        CompletableFuture[] futures = new CompletableFuture[(int)times];
+        CompletableFuture[] futures = new CompletableFuture[(int) times];
         for (int i = 1; i <= times; i++) {
             PageHelper.startPage(i, pageSize);
             List<Object> list = query();
@@ -55,7 +53,7 @@ public class PageDemo {
                 pageSw.stop();
                 log.info("pageHelper pageNum:{} cost:{}s", finalI, pageSw.getTotalTimeSeconds());
             }, threadPool).exceptionally(throwable -> {
-                log.error("pageHelper pageNum:{} error", finalI, throwable);
+                log.error("pageHelper pageNum:{} error:{}", finalI, ExceptionUtils.getStackTrace(throwable));
                 return null;
             });
         }
@@ -65,99 +63,51 @@ public class PageDemo {
         log.info("pageHelper end cost:{}m", sw.getTotalTimeSeconds() / 60);
     }
 
-    public static void pageHelperDealSelfSync(int pageSize) {
+    public static void updateByPageHelper(int pageSize) {
         Page<Object> page = PageHelper.startPage(1, 1);
         query();
         long count = page.getTotal();
         if (count == 0) {
             return;
         }
-        List<Object> list;
         long times = (count % pageSize == 0) ? count / pageSize : (count / pageSize + 1);
         for (int i = 1; i <= times; i++) {
             PageHelper.startPage(1, pageSize);
-            list = query();
-            // TODO: 处理业务逻辑
-        }
-    }
-
-    public static void pageHelperDealSelfAsync(int pageSize) {
-        Page<Object> checkPage = PageHelper.startPage(1, 1);
-        query();
-        long count = checkPage.getTotal();
-        if (count == 0) {
-            return;
-        }
-        List<Object> preList = new ArrayList<>();
-        List<Object> postList = new ArrayList<>();
-        long times = (count % pageSize == 0) ? count / pageSize : (count / pageSize + 1);
-        for (int i = 1; i <= times; i++) {
-            PageHelper.startPage(i, pageSize);
-            List<Object> list = query();
-            preList.addAll(list);
-        }
-        preList.forEach(object -> {
-            postList.add(object);
-            if (postList.size() >= pageSize) {
-                List<Object> lists = new ArrayList<>(postList);
-                postList.clear();
-                CompletableFuture.runAsync(() -> lists.forEach(o -> {
-                    // TODO: 处理业务逻辑
-                }));
-            }
-        });
-        if (!postList.isEmpty()) {
-            CompletableFuture.runAsync(() -> postList.forEach(o -> {
+            query().forEach(item -> {
                 // TODO: 处理业务逻辑
-            }));
+            });
         }
     }
 
     public static void whilePage(int pageSize) {
-        log.info("whilePage start");
-        List<CompletableFuture> futures = new ArrayList<>();
-        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(3, 3, 600L, TimeUnit.MICROSECONDS,
-                new LinkedBlockingQueue<>(), new ThreadFactoryBuilder().setNameFormat("whilePage-pool-%d").build());
+        log.info("whilePage start pageSize:{}", pageSize);
         int pageNum = 1;
         StopWatch sw = new StopWatch();
         sw.start();
         while (true) {
             List<Object> list = pageQuery(pageNum, pageSize);
-            if (CollectionUtils.isEmpty(list)) {
+            log.info("whilePage pageNum:{} size:{}", pageNum, list.size());
+            list.forEach(ordersResponse -> {
+                // TODO: 处理业务逻辑
+            });
+            if (list.size() < pageSize) {
                 break;
             }
-            int finalPageNum = pageNum;
-            futures.add(CompletableFuture.runAsync(() -> {
-                StopWatch pageSw = new StopWatch();
-                pageSw.start();
-                list.forEach(ordersResponse -> {
-                    // TODO: 处理业务逻辑
-                });
-                pageSw.stop();
-                log.info("whilePage pageNum:{} cost:{}s", finalPageNum, pageSw.getTotalTimeSeconds());
-            }, threadPool).exceptionally(throwable -> {
-                log.error("whilePage pageNum:{} error", finalPageNum, throwable);
-                return null;
-            }));
             pageNum++;
         }
-        CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()])).join();
-        threadPool.shutdown();
         sw.stop();
         log.info("whilePage end cost:{}m", sw.getTotalTimeSeconds() / 60);
     }
 
-    public static List<Object> page(List<Object> list, int pageNum, int pageSize) {
-        if (CollectionUtils.isEmpty(list)) {
-            return Collections.emptyList();
-        }
-        int size = list.size();
-        int start = (pageNum - 1) * pageSize > size ? size : (pageNum - 1) * pageSize;
-        int end = pageSize * pageNum > size ? size : pageSize * pageNum;
-        return new ArrayList<>(list.subList(start, end));
+    public static List<Object> listPage(List<Object> list, int pageNum, int pageSize) {
+        return CollUtil.page(pageNum - 1, pageSize, list);
+//        int size = list.size();
+//        int start = (pageNum - 1) * pageSize > size ? size : (pageNum - 1) * pageSize;
+//        int end = pageSize * pageNum > size ? size : pageSize * pageNum;
+//        return new ArrayList<>(list.subList(start, end));
     }
 
-    public static void partition(List<Object> list, int size) {
+    public static void listPartition(List<Object> list, int size) {
         List<List<Object>> partition = ListUtils.partition(list, size);
         partition.forEach(objects -> {
             // TODO: 处理业务逻辑
