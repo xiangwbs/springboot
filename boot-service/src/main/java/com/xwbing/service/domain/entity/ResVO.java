c1957cd627cc2cdde7a468f7e4e4d68778438457
xiangwb <xiangwbs@163.com>
2024-02-18 09:35:53 +0800

    FIX

--- boot-service/src/main/java/com/xwbing/service/domain/entity/ResVO.java
+++ boot-service/src/main/java/com/xwbing/service/domain/entity/ResVO.java
@@ -1,5 +1,8 @@
 package com.xwbing.service.domain.entity;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.xwbing.service.domain.entity.sys.SysUser;
 import com.xwbing.service.exception.BusinessException;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -16,12 +19,21 @@ public class ResVO<T> {
     private String msg;
     private T data;
 
-    public static <T> T getData(ResVO<T> res) {
+    public static <T> T checkRes(ResVO<T> res) {
         String code = res.getCode();
         if (!"200".equals(code)) {
             log.error("code:{} msg:{}", res.getCode(), res.getMsg());
             throw new BusinessException(res.getMsg());
         }
         return res.getData();
+    }
+
+    public static boolean isSuccess(ResVO resVO) {
+        return resVO != null && "200".equals(resVO.getCode());
+    }
+
+    public static void main(String[] args) {
+        ResVO<SysUser> sysUserResVO = JSON.parseObject("", new TypeReference<ResVO<SysUser>>() {
+        });
     }
 }
