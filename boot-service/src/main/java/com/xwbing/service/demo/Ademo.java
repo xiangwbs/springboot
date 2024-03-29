4233215c9ffc2ff056c414b63c31980581b53188
xiangwb <xiangwbs@163.com>
2024-03-29 16:52:57 +0800

    fix

--- boot-service/src/main/java/com/xwbing/service/demo/Ademo.java
+++ boot-service/src/main/java/com/xwbing/service/demo/Ademo.java
@@ -1,13 +1,102 @@
 package com.xwbing.service.demo;
 
-import lombok.extern.java.Log;
+import com.alibaba.dashscope.app.Application;
+import com.alibaba.dashscope.app.ApplicationParam;
+import com.alibaba.dashscope.app.ApplicationResult;
+import com.alibaba.dashscope.exception.InputRequiredException;
+import com.alibaba.dashscope.exception.NoApiKeyException;
+import com.aliyun.broadscope.bailian.sdk.AccessTokenClient;
+import com.aliyun.broadscope.bailian.sdk.ApplicationClient;
+import com.aliyun.broadscope.bailian.sdk.BaiLianSdkException;
+import com.aliyun.broadscope.bailian.sdk.models.*;
+import io.reactivex.Flowable;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import reactor.core.publisher.Flux;
 
-/**
- * 项目名称: boot-module-pro
- * 创建时间: 2018/1/23 14:45
- * 作者: xiangwb
- * 说明: 测试用
- */
-@Log
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+
 public class Ademo {
+    public static void main(String[] args) throws NoApiKeyException, InputRequiredException {
+//        testCompletionsWithStream();
+        streamCall();
+
+    }
+
+    public static void streamCall() throws NoApiKeyException, InputRequiredException {
+        ApplicationParam param = ApplicationParam.builder()
+                .apiKey("xxx")
+                .appId("xxx")
+                .prompt("如何做土豆炖猪脚?")
+                .build();
+
+        Application application = new Application();
+        Flowable<ApplicationResult> result = application.streamCall(param);
+        result.blockingForEach(data -> {
+            System.out.printf("requestId: %s, text: %s, finishReason: %s\n",
+                    data.getRequestId(), data.getOutput().getText(), data.getOutput().getFinishReason());
+
+        });
+    }
+
+    /**
+     * 流式响应使用示例
+     */
+    public static void testCompletionsWithStream() {
+        String accessKeyId = "xxx";
+        String accessKeySecret = "xxx";
+        String agentKey = "xxx";
+        String appId = "xxx";
+
+        AccessTokenClient accessTokenClient = new AccessTokenClient(accessKeyId, accessKeySecret, agentKey);
+        String token = accessTokenClient.getToken();
+        ApplicationClient client = ApplicationClient.builder()
+                .token(token)
+                .build();
+
+        List<ChatRequestMessage> messages = new ArrayList<>();
+        messages.add(new ChatSystemMessage("你是一名天文学家, 能够帮助小学生回答宇宙与天文方面的问题"));
+        messages.add(new ChatUserMessage("宇宙中为什么会存在黑洞"));
+
+        CompletionsRequest request = new CompletionsRequest()
+                .setAppId(appId)
+                .setMessages(messages)
+                .setParameters(new CompletionsRequest.Parameter()
+                        //开启增量输出模式，后面输出不会包含已经输出的内容
+                        .setIncrementalOutput(true)
+                        //返回choice message结果
+                        .setResultFormat("message")
+                );
+
+        CountDownLatch latch = new CountDownLatch(1);
+        Flux<CompletionsResponse> response = client.streamCompletions(request);
+
+        response.subscribe(
+                data -> {
+                    if (data.isSuccess()) {
+                        System.out.printf("%s", data.getData().getChoices().get(0).getMessage().getContent());
+                    } else {
+                        System.out.printf("failed to create completion, requestId: %s, code: %s, message: %s\n",
+                                data.getRequestId(), data.getCode(), data.getMessage());
+                    }
+                },
+                err -> {
+                    System.out.printf("failed to create completion, err: %s\n", ExceptionUtils.getStackTrace(err));
+                    latch.countDown();
+                },
+                () -> {
+                    System.out.println("\ncreate completion completely");
+                    latch.countDown();
+                }
+        );
+
+        try {
+            latch.await(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new BaiLianSdkException(e);
+        }
+    }
 }
