eeb346be3154bd22d5dddb7e24358baf5c20dd66
xiangwb <xiangwbs@163.com>
2024-06-25 17:36:12 +0800

    fix

--- boot-service/src/main/java/com/xwbing/service/demo/sql/JdbcDemo.java
+++ boot-service/src/main/java/com/xwbing/service/demo/sql/JdbcDemo.java
@@ -12,12 +12,9 @@ import java.util.*;
  */
 @Slf4j
 public class JdbcDemo {
-    public static List<Map<String, Object>> query(String sql) {
-        String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/boot";
-        String username = "root";
-        String password = "xiangwbs";
+    public static List<Map<String, Object>> queryData(String url, String username, String password, String sql) {
         List<Map<String, Object>> list = new ArrayList<>();
-        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
             ResultSetMetaData metaData = resultSet.getMetaData();
@@ -33,13 +30,54 @@ public class JdbcDemo {
             statement.close();
             return list;
         } catch (SQLException e) {
-            log.error("jdbcUtil query sql:{} error", sql, e);
+            log.error("jdbcUtil queryData sql:{} error", sql, e);
+            return Collections.emptyList();
+        }
+    }
+
+    public static List<Map<String, Object>> queryColumn(String url, String username, String password, String tableName) {
+        List<Map<String, Object>> list = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            Statement statement = connection.createStatement();
+            String sql = null;
+            if (url.contains(":mysql:")) {
+                String schema = url.replaceAll("jdbc:mysql://", "");
+                schema = schema.substring(schema.indexOf('/') + 1);
+                if (schema.contains("?")) {
+                    schema = schema.substring(0, schema.indexOf('?'));
+                }
+                sql = "SELECT TABLE_NAME tableCode,COLUMN_NAME code,COLUMN_COMMENT name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '" + schema + "' AND TABLE_NAME = '" + tableName + "'";
+            } else if (url.contains(":dm:")) {
+                sql = "SELECT TABLE_NAME tableCode,COLUMN_NAME code,COMMENTS name FROM user_col_comments WHERE TABLE_NAME = '" + tableName.toUpperCase() + "'";
+            }
+            if (sql == null) {
+                return Collections.emptyList();
+            }
+            ResultSet resultSet = statement.executeQuery(sql);
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnsCount = metaData.getColumnCount();
+            while (resultSet.next()) {
+                Map<String, Object> column = new LinkedHashMap<>();
+                for (int i = 1; i <= columnsCount; i++) {
+                    column.put(metaData.getColumnLabel(i).toLowerCase(), resultSet.getString(i).toLowerCase());
+                }
+                list.add(column);
+            }
+            resultSet.close();
+            statement.close();
+            return list;
+        } catch (SQLException e) {
+            log.error("jdbcUtil queryColumn  error", e);
             return Collections.emptyList();
         }
     }
 
     public static void main(String[] args) {
-        List<Map<String, Object>> query = query("SELECT * from sys_user_info");
+        String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/boot";
+        String username = "root";
+        String password = "xiangwbs";
+        List<Map<String, Object>> dataList = queryData(jdbcUrl, username, password, "SELECT * from sys_user_info");
+//        List<Map<String, Object>> columnList = queryColumn(jdbcUrl, username, password, "bot_chat_bi_table");
         System.out.println("");
     }
 }
