f1c44e37bf3134556dd9bf54c11934712b1d6eac
xiangwb <xiangwbs@163.com>
2024-07-23 10:02:44 +0800

    fix

---
+++ boot-service/src/main/java/com/xwbing/service/domain/entity/dto/DocDemoDTO.java
@@ -0,0 +1,17 @@
+package com.xwbing.service.domain.entity.dto;
+
+import lombok.Data;
+
+/**
+ * @author daofeng
+ * @version <a href="https://www.baidu.com">baidu</a>，
+ * @since 2024年07月23日 9:49 AM
+ */
+@Data
+public class DocDemoDTO {
+    /**
+     * @see com.xwbing.service.enums.YesOrNoEnum#getCode()
+     * {@link com.xwbing.service.enums.YesOrNoEnum#YES}
+     */
+    private String yesOrNo;
+}
