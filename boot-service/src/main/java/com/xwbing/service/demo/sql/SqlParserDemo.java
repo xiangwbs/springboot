7d13e5f30352d3145e6218783e65c2f05db5ea7e
xiangwb <xiangwbs@163.com>
2024-08-23 14:29:32 +0800

    fix

--- boot-service/src/main/java/com/xwbing/service/demo/sql/SqlParserDemo.java
+++ boot-service/src/main/java/com/xwbing/service/demo/sql/SqlParserDemo.java
@@ -3,7 +3,6 @@ package com.xwbing.service.demo.sql;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.expression.Alias;
 import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.ExpressionVisitorAdapter;
 import net.sf.jsqlparser.expression.operators.relational.Between;
 import net.sf.jsqlparser.expression.operators.relational.ComparisonOperator;
 import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
@@ -13,7 +12,6 @@ import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.Statement;
 import net.sf.jsqlparser.statement.select.*;
-import net.sf.jsqlparser.util.SelectUtils;
 import net.sf.jsqlparser.util.TablesNamesFinder;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -31,18 +29,7 @@ import java.util.stream.Collectors;
 public class SqlParserDemo {
     public static void main(String[] args) throws Exception {
         base("select distinct r.name,count(a.id) from ROLE r left join AUTHORITY a on(r.id=a.roleId) where r.id in(1000,1001) group by r.id having count(a.id)>10 order by r.creationDate desc limit 10");
-//        String addColumnSql = SqlUtil.addColumn("select name,age from user where id=1", "sex");
 //        formatDate("select region from region_data where date is not null and date!='2023' and (date in('2023','2024') and date between '2023' and '2024')");
-//        List<String> fieldList = SqlUtil.listField("select region from region_data where date is not null and date!='2023' and (code in('2023','2024') and age between '10' and '20') group by code order by id desc");
-//        String formatSql = SqlUtil.formatSql("select yearmonth,mofdivname,incexptype,budgetsubjectcode,budgetsubjectname,amt from dws_budget_domain_srzc_ai where mofdivname in ('余杭区', '淳安县') and budgetsubjectname like '%干部教育%' and incexptype = '1' order by amt desc group by mofdivname limit 1000");
-
-//        SqlFieldVO sqlField = new SqlFieldVO();
-//        sqlField.setCode("a");
-//        sqlField.setName("我是a");
-//        SqlFieldVO sqlField1 = new SqlFieldVO();
-//        sqlField1.setCode("b");
-//        sqlField1.setName("我是b");
-//        dealSql("表", "select a as `第一`,b from table1 where a=1 and b=1 group by `第一` having `第一`>0 order by `第一` limit 10", ListUtil.toList(sqlField, sqlField1), false);
         System.out.println("");
     }
 
@@ -69,7 +56,6 @@ public class SqlParserDemo {
         Limit limit = select.getLimit();
         System.out.println("");
     }
-
 
     private static Map<String, String> formatDate(String sql) {
         Map<String, String> sqlMap = new HashMap<>();
@@ -106,120 +92,6 @@ public class SqlParserDemo {
                     }
                 });
         return sqlMap;
-    }
-
-    private static DealSqlVO dealSql(String tableName, String sql, List<SqlFieldVO> fieldList, boolean addLimit) {
-        PlainSelect select = SqlUtil.getSelect(sql);
-        if (select == null) {
-            return null;
-        }
-        DealSqlVO vo = new DealSqlVO();
-        vo.setOriginalSql(sql);
-        List<String> selectFieldList = new ArrayList<>();
-        Map<String, Byte> functionDataTypeMap = new HashMap<>();
-        Map<String, String> fieldNameMap = fieldList.stream().collect(Collectors.toMap(SqlFieldVO::getCode, SqlFieldVO::getName));
-        Map<String, String> columnAliasMap = new HashMap<>();
-        List<SelectItem<?>> selectItems = select.getSelectItems();
-        if (selectItems.size() == 1 && selectItems.get(0).getExpression() instanceof AllColumns) {
-            // select * 替换成字段
-            selectItems.remove(0);
-            for (SqlFieldVO sqlField : fieldList) {
-                SelectUtils.addExpression(select, new Column(sqlField.getCode()));
-            }
-        }
-        selectItems.forEach(selectItem -> {
-            Expression expression = selectItem.getExpression();
-            if (expression instanceof Column) {
-                Column column = (Column) expression;
-                // 设置别名
-                Alias alias = selectItem.getAlias();
-                if (alias == null) {
-                    String columnName = fieldNameMap.get(column.getColumnName());
-                    if (StringUtils.isNotEmpty(columnName)) {
-                        alias = new Alias("`" + columnName + "`", true);
-                        selectItem.setAlias(alias);
-                    }
-                }
-                if (alias != null) {
-                    columnAliasMap.put(alias.getName(), column.getColumnName());
-                }
-                // 汇总查询字段
-                selectFieldList.add(column.getColumnName());
-            } else {
-                // 映射涉及计算列的字段类型
-                String expressionStr = expression.toString();
-                String key = Optional.ofNullable(selectItem.getAlias()).map(Alias::getName).orElse(expressionStr);
-                fieldList.stream().filter(chatBiFieldVO -> expressionStr.contains(chatBiFieldVO.getCode())).findFirst().ifPresent(matchField -> functionDataTypeMap.put(key, matchField.getDataType()));
-                // 汇总查询列
-                selectFieldList.add(key);
-            }
-        });
-        String aliasSql = select.toString().toLowerCase();
-        if (addLimit) {
-            Limit limit = select.getLimit();
-            if (limit == null) {
-                aliasSql = aliasSql + " limit 1000";
-            }
-        }
-        vo.setAliasSql(aliasSql);
-        vo.setSelectFieldList(selectFieldList);
-        vo.setFunctionDataTypeMap(functionDataTypeMap);
-        // 获取去除别名的sql 便于动态sql查询到的数据(map<key,value>)key能匹配到对应的字段
-        select = SqlUtil.getSelect(aliasSql);
-        select.getSelectItems().forEach(selectItem -> {
-            Expression expression = selectItem.getExpression();
-            if (expression instanceof Column) {
-                selectItem.setAlias(null);
-            }
-        });
-        GroupByElement groupBy = select.getGroupBy();
-        if (groupBy != null) {
-            groupBy.getGroupByExpressionList().forEach(expression -> {
-                if (expression instanceof Column) {
-                    Column column = (Column) expression;
-                    String columnName = columnAliasMap.get(column.getColumnName());
-                    if (StringUtils.isNotEmpty(columnName)) {
-                        column.setColumnName(columnName);
-                    }
-                }
-            });
-        }
-        Expression having = select.getHaving();
-        if (having != null) {
-            having.accept(new ExpressionVisitorAdapter() {
-                @Override
-                public void visit(Column column) {
-                    String columnName = columnAliasMap.get(column.getColumnName());
-                    if (StringUtils.isNotEmpty(columnName)) {
-                        column.setColumnName(columnName);
-                    }
-                }
-            });
-        }
-        List<OrderByElement> orderByElements = select.getOrderByElements();
-        if (CollectionUtils.isNotEmpty(orderByElements)) {
-            orderByElements.forEach(orderByElement -> {
-                Expression expression = orderByElement.getExpression();
-                if (expression instanceof Column) {
-                    Column column = (Column) expression;
-                    String columnName = columnAliasMap.get(column.getColumnName());
-                    if (StringUtils.isNotEmpty(columnName)) {
-                        column.setColumnName(columnName);
-                    }
-                }
-            });
-        }
-        vo.setNoAliasSql(select.toString().toLowerCase());
-        // 加工展示的sql
-        select = SqlUtil.getSelect(aliasSql);
-        Table fromTable = (Table) select.getFromItem();
-        fromTable.setName("`" + tableName + "`");
-        String displaySql = select.toString().toLowerCase();
-        for (SqlFieldVO field : fieldList) {
-            displaySql = displaySql.replaceAll("\\b" + field.getCode() + "\\b", "`" + field.getName() + "`");
-        }
-        vo.setDisplaySql(displaySql);
-        return vo;
     }
 
     public static String reSql(String sql) {
--- boot-service/src/main/java/com/xwbing/service/demo/sql/SqlUtil.java
+++ boot-service/src/main/java/com/xwbing/service/demo/sql/SqlUtil.java
@@ -5,26 +5,19 @@ import com.github.vertical_blank.sqlformatter.SqlFormatter;
 import com.github.vertical_blank.sqlformatter.core.FormatConfig;
 import com.github.vertical_blank.sqlformatter.languages.Dialect;
 import lombok.extern.slf4j.Slf4j;
-import net.sf.jsqlparser.expression.BinaryExpression;
-import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.ExpressionVisitorAdapter;
-import net.sf.jsqlparser.expression.Parenthesis;
+import net.sf.jsqlparser.expression.*;
 import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
 import net.sf.jsqlparser.expression.operators.conditional.OrExpression;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.Statement;
-import net.sf.jsqlparser.statement.select.GroupByElement;
-import net.sf.jsqlparser.statement.select.OrderByElement;
-import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.*;
 import net.sf.jsqlparser.util.SelectUtils;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -146,10 +139,131 @@ public class SqlUtil {
         return SqlFormatter.of(Dialect.MySql).format(sql, formatConfig);
     }
 
+    public static DealSqlVO dealSql(String tableName, String sql, List<SqlFieldVO> fieldList, boolean addLimit) {
+        PlainSelect select = SqlUtil.getSelect(sql);
+        if (select == null) {
+            return null;
+        }
+        DealSqlVO vo = new DealSqlVO();
+        vo.setOriginalSql(sql);
+        List<String> selectFieldList = new ArrayList<>();
+        Map<String, Byte> functionDataTypeMap = new HashMap<>();
+        Map<String, String> fieldNameMap = fieldList.stream().collect(Collectors.toMap(SqlFieldVO::getCode, SqlFieldVO::getName));
+        Map<String, String> columnAliasMap = new HashMap<>();
+        List<SelectItem<?>> selectItems = select.getSelectItems();
+        if (selectItems.size() == 1 && selectItems.get(0).getExpression() instanceof AllColumns) {
+            // select * 替换成字段
+            selectItems.remove(0);
+            for (SqlFieldVO sqlField : fieldList) {
+                SelectUtils.addExpression(select, new Column(sqlField.getCode()));
+            }
+        }
+        selectItems.forEach(selectItem -> {
+            Expression expression = selectItem.getExpression();
+            if (expression instanceof Column) {
+                Column column = (Column) expression;
+                // 设置别名
+                Alias alias = selectItem.getAlias();
+                if (alias == null) {
+                    String columnName = fieldNameMap.get(column.getColumnName());
+                    if (StringUtils.isNotEmpty(columnName)) {
+                        alias = new Alias("`" + columnName + "`", true);
+                        selectItem.setAlias(alias);
+                    }
+                }
+                if (alias != null) {
+                    columnAliasMap.put(alias.getName(), column.getColumnName());
+                }
+                // 汇总查询字段
+                selectFieldList.add(column.getColumnName());
+            } else {
+                // 映射涉及计算列的字段类型
+                String expressionStr = expression.toString();
+                String key = Optional.ofNullable(selectItem.getAlias()).map(Alias::getName).orElse(expressionStr);
+                fieldList.stream().filter(chatBiFieldVO -> expressionStr.contains(chatBiFieldVO.getCode())).findFirst().ifPresent(matchField -> functionDataTypeMap.put(key, matchField.getDataType()));
+                // 汇总查询列
+                selectFieldList.add(key);
+            }
+        });
+        String aliasSql = select.toString().toLowerCase();
+        if (addLimit) {
+            Limit limit = select.getLimit();
+            if (limit == null) {
+                aliasSql = aliasSql + " limit 1000";
+            }
+        }
+        vo.setAliasSql(aliasSql);
+        vo.setSelectFieldList(selectFieldList);
+        vo.setFunctionDataTypeMap(functionDataTypeMap);
+        // 获取去除别名的sql 便于动态sql查询到的数据(map<key,value>)key能匹配到对应的字段
+        select = SqlUtil.getSelect(aliasSql);
+        select.getSelectItems().forEach(selectItem -> {
+            Expression expression = selectItem.getExpression();
+            if (expression instanceof Column) {
+                selectItem.setAlias(null);
+            }
+        });
+        GroupByElement groupBy = select.getGroupBy();
+        if (groupBy != null) {
+            groupBy.getGroupByExpressionList().forEach(expression -> {
+                if (expression instanceof Column) {
+                    Column column = (Column) expression;
+                    String columnName = columnAliasMap.get(column.getColumnName());
+                    if (StringUtils.isNotEmpty(columnName)) {
+                        column.setColumnName(columnName);
+                    }
+                }
+            });
+        }
+        Expression having = select.getHaving();
+        if (having != null) {
+            having.accept(new ExpressionVisitorAdapter() {
+                @Override
+                public void visit(Column column) {
+                    String columnName = columnAliasMap.get(column.getColumnName());
+                    if (StringUtils.isNotEmpty(columnName)) {
+                        column.setColumnName(columnName);
+                    }
+                }
+            });
+        }
+        List<OrderByElement> orderByElements = select.getOrderByElements();
+        if (org.apache.commons.collections4.CollectionUtils.isNotEmpty(orderByElements)) {
+            orderByElements.forEach(orderByElement -> {
+                Expression expression = orderByElement.getExpression();
+                if (expression instanceof Column) {
+                    Column column = (Column) expression;
+                    String columnName = columnAliasMap.get(column.getColumnName());
+                    if (StringUtils.isNotEmpty(columnName)) {
+                        column.setColumnName(columnName);
+                    }
+                }
+            });
+        }
+        vo.setNoAliasSql(select.toString().toLowerCase());
+        // 加工展示的sql
+        select = SqlUtil.getSelect(aliasSql);
+        Table fromTable = (Table) select.getFromItem();
+        fromTable.setName("`" + tableName + "`");
+        String displaySql = select.toString().toLowerCase();
+        for (SqlFieldVO field : fieldList) {
+            displaySql = displaySql.replaceAll("\\b" + field.getCode() + "\\b", "`" + field.getName() + "`");
+        }
+        vo.setDisplaySql(displaySql);
+        return vo;
+    }
+
     public static void main(String[] args) {
-//        String sql = "select a as `a`,b,c from table1 where a=1 and b=1 and (c between 1 and 2) group by a order by b limit 10;select * from table2";
-//        String formatSql = formatSql(sql);
-        List<String> fieldList = listField("select a from table where b=1 group by c order by d");
+//        String formatSql = formatSql("select a as `a`,b,c from table1 where a=1 and b=1 and (c between 1 and 2) group by a order by b limit 10;select * from table2");
+//        List<String> fieldList = SqlUtil.listField("select region from region_data where date is not null and date!='2023' and (code in('2023','2024') and age between '10' and '20') group by code order by id desc");
+//        String addColumnSql = SqlUtil.addColumn("select name,age from user where id=1", "sex");
+        SqlFieldVO sqlField = new SqlFieldVO();
+        sqlField.setCode("a");
+        sqlField.setName("我是a");
+        SqlFieldVO sqlField1 = new SqlFieldVO();
+        sqlField1.setCode("b");
+        sqlField1.setName("我是b");
+        SqlUtil.dealSql("表", "select a as `第一`,b from table1 where a=1 and b=1 group by `第一` having `第一`>0 order by `第一` limit 10", ListUtil.toList(sqlField, sqlField1), false);
         System.out.println("");
     }
 }
