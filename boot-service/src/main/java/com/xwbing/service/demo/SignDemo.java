9ae5e811e76532e0674aeb1c0855f37550153234
xiangwb <xiangwbs@163.com>
2025-03-21 15:41:44 +0800

    授权认证demo

---
+++ boot-service/src/main/java/com/xwbing/service/demo/SignDemo.java
@@ -0,0 +1,38 @@
+package com.xwbing.service.demo;
+
+import cn.hutool.crypto.digest.DigestUtil;
+import cn.hutool.http.HttpRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author daofeng
+ * @version $
+ * @since 2025年03月21日 15:33
+ */
+public class SignDemo {
+
+    public static void auth() {
+        String appKey = "";
+        String appSecret = "";
+        String appToken = "";
+        long timestamp = System.currentTimeMillis();
+        String sign = DigestUtil.md5Hex(appKey + appSecret + appToken + timestamp);
+        Map<String, Object> param = new HashMap<>();
+        param.put("appKey", appKey);
+        param.put("sign", sign);
+        param.put("timestamp", timestamp);
+        String res = HttpRequest
+                .get("")
+                .form(param)
+                .header("appToken", appToken)
+                .execute()
+                .body();
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
