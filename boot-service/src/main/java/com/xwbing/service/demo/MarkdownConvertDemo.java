9a0388fe5678cfd0e196e089f57cea9240a26d00
xiangwb <xiangwbs@163.com>
2025-10-21 15:32:35 +0800

    markdown

--- boot-service/src/main/java/com/xwbing/service/demo/MarkdownConvertDemo.java
+++ boot-service/src/main/java/com/xwbing/service/demo/MarkdownConvertDemo.java
@@ -17,60 +17,61 @@ import java.util.stream.IntStream;
 public class MarkdownConvertDemo {
     public static void main(String[] args) {
         String s = "[\n" +
-                "    {\n" +
-                "      \"年度\": 2020,\n" +
-                "      \"单位代码\": 407007,\n" +
-                "      \"单位名称\": \"杭州博物馆（杭州博物院（筹））\",\n" +
-                "      \"全年预算数\": 7787557,\n" +
-                "      \"决算数\": 7080222.72\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"年度\": 2021,\n" +
-                "      \"单位代码\": 407007,\n" +
-                "      \"单位名称\": \"杭州博物馆（杭州博物院（筹））\",\n" +
-                "      \"全年预算数\": 32780354.1,\n" +
-                "      \"决算数\": 32330825.02\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"年度\": 2022,\n" +
-                "      \"单位代码\": 407007,\n" +
-                "      \"单位名称\": \"杭州博物馆（杭州博物院（筹））\",\n" +
-                "      \"全年预算数\": 34804225.2,\n" +
-                "      \"决算数\": 34412790.59\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"年度\": 2023,\n" +
-                "      \"单位代码\": 407007,\n" +
-                "      \"单位名称\": \"杭州博物馆（杭州博物院（筹））\",\n" +
-                "      \"全年预算数\": 40526032.25,\n" +
-                "      \"决算数\": 37858800.13\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"年度\": 2024,\n" +
-                "      \"单位代码\": 407007,\n" +
-                "      \"单位名称\": \"杭州博物馆（杭州博物院（筹））\",\n" +
-                "      \"全年预算数\": 40433508.94,\n" +
-                "      \"决算数\": 40237337.68\n" +
-                "    }\n" +
-                "  ]";
+                "  {\n" +
+                "    \"年度\": 2024,\n" +
+                "    \"项目类别\": \"基本支出\",\n" +
+                "    \"全年预算数\": 19137901.94,\n" +
+                "    \"决算数\": 19092134.55\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"年度\": 2024,\n" +
+                "    \"项目类别\": \"项目支出\",\n" +
+                "    \"全年预算数\": 21295607,\n" +
+                "    \"决算数\": 21145203.13\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"年度\": 2022,\n" +
+                "    \"项目类别\": \"基本支出\",\n" +
+                "    \"全年预算数\": 12634142,\n" +
+                "    \"决算数\": 12384193.79\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"年度\": 2022,\n" +
+                "    \"项目类别\": \"项目支出\",\n" +
+                "    \"全年预算数\": 22170083.2,\n" +
+                "    \"决算数\": 22028596.8\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"年度\": 2021,\n" +
+                "    \"项目类别\": \"基本支出\",\n" +
+                "    \"全年预算数\": 13002586.1,\n" +
+                "    \"决算数\": 12795512.77\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"年度\": 2021,\n" +
+                "    \"项目类别\": \"项目支出\",\n" +
+                "    \"全年预算数\": 19777768,\n" +
+                "    \"决算数\": 19535312.25\n" +
+                "  }\n" +
+                "]";
         JSONArray objects = JSONUtil.parseArray(s);
-        String s1 = markdownExcel(ListUtil.toList("年度", "全年预算数", "决算数"), objects);
+        String s1 = markdown(ListUtil.toList("年度","项目类别", "全年预算数", "决算数"), objects);
         System.out.println("");
     }
 
-    private static String markdownExcel(List<String> heads, JSONArray dataArray) {
-        String head = String.join("|", heads);
-        head = "| " + head + " |";
+    private static String markdown(List<String> heads, JSONArray dataArray) {
+        String headStr = String.join("|", heads);
+        headStr = "|" + headStr + "|";
         String separator = IntStream
                 .range(0, heads.size())
                 .mapToObj(i -> "---")
-                .collect(Collectors.joining(" | "));
-        separator = "| " + separator + " |";
+                .collect(Collectors.joining("|"));
+        separator = "|" + separator + "|";
         String datasStr = dataArray.stream().map(data -> {
             JSONObject dataObj = JSONUtil.parseObj(JSONUtil.toJsonStr(data));
-            String dataStr = heads.stream().map(dataObj::getStr).collect(Collectors.joining(" | "));
-            return "| " + dataStr + " |";
+            String dataStr = heads.stream().map(dataObj::getStr).collect(Collectors.joining("|"));
+            return "|" + dataStr + "|";
         }).collect(Collectors.joining("\n"));
-        return head + "\n" + separator + "\n" + datasStr;
+        return headStr + "\n" + separator + "\n" + datasStr;
     }
 }
