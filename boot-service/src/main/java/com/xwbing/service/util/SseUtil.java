1d444a525953a6f0b9121145598754153049cd36
xiangwb <xiangwbs@163.com>
2026-09-04 15:25:19 +0800

    fix

--- boot-service/src/main/java/com/xwbing/service/util/SseUtil.java
+++ boot-service/src/main/java/com/xwbing/service/util/SseUtil.java
@@ -88,7 +88,7 @@ public class SseUtil {
             TrustManager[] trustAllCerts = new TrustManager[]{
                     new X509TrustManager() {
                         public X509Certificate[] getAcceptedIssuers() {
-                            return null;
+                            return new X509Certificate[0];
                         }
 
                         public void checkClientTrusted(X509Certificate[] certs, String authType) {
